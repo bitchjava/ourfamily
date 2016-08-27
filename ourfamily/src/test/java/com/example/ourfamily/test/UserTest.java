9d9ff82bf4594bc1dd3ed67e098209cd33d0cb48
zhangming01 <510097152@qq.com>
2016-08-27 22:11:47 +0800

    1 pom.xml 2 测试代码

--- ourfamily/src/test/java/com/example/ourfamily/test/UserTest.java
+++ ourfamily/src/test/java/com/example/ourfamily/test/UserTest.java
@@ -1,12 +1,10 @@
 package com.example.ourfamily.test;
 
-import static org.junit.Assert.*;  
-
-import org.junit.Test;  
-import org.junit.runner.RunWith;  
-import org.springframework.beans.factory.annotation.Autowired;  
-import org.springframework.test.context.ContextConfiguration;  
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;  
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.ourfamily.model.User;
 
